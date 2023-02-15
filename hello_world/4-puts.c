e0e93deaf129671afade7ad5f277c51f1cbff441
Josue <joshermo15@gmail.com>
2023-02-14 22:56:36 -0800

    Task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
-
 /**
- *main - prints "Programming is like building a multilingual
- *puzzle, followed by a newline."
- *return: 0.
+ * main - main block
+ *return: 0
  */
-
-int main (void)
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like develop a multilingual puzzle");
   return (0);
 }
