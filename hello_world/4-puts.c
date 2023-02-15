3318059ed89634429494b8558ce68e7bfc32bd3f
Josue <joshermo15@gmail.com>
2023-02-15 15:15:35 -0800

    Task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- *Return: Always 0
- */
+*main - A Program that prints a line with puts fuction
+*Return: 0 (Success)
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-       
+puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
