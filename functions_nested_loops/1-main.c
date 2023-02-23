6bdb43ba7aa67d77bb6ef38e0993f5305d96fa42
Josue <joshermo15@gmail.com>
2023-02-22 17:17:12 -0800

    Task1

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -2,9 +2,9 @@
 /**
  * main - check the code
  * Return: Always 0.
- */
+ */ 
 int main(void)
 {
-prin_alphabet();
+print_alphabet();
 return (0);
 }
--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return: On success 1.
- * 0n error, -1is returned, and errno is set appropiately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
