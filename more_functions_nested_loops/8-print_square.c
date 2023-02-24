e438fbb0ed45faf136963a1fd52cb24476f885f2
Josue <joshermo15@gmail.com>
2023-02-24 10:25:06 -0800

    Task8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_square - Prints n squares according n number of times
- * @Size: The number of squares/number of times
+ * @size: The number of squares/number of times
  * Return: empty
  */
 void print_square(int size)
